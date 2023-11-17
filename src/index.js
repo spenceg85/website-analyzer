75b1904a562d15fdcdc896ae1925f9b7217cb058
Spencer Grier <spenceg85@yahoo.com>
2023-11-17 16:03:07 -0500

    Update dependencies and refactor analyze-website endpoint

--- src/index.js
+++ src/index.js
@@ -10,22 +10,77 @@ app.use(express.json());
 
 app.post("/analyze-website", async (req, res) => {
   try {
-    const { websiteUrl } = req.body;
+    const websiteUrl = req.body?.url;
+    // console.log("req: ", req.body?.url);
     if (!websiteUrl) {
-      return res.status(400).send({ error: "Website URL is required" });
+      return res.status(400).send({
+        error: `Website URL is required: ${websiteUrl}, body: ${req.body}`,
+      });
     }
 
     // Perform your website analysis logic here
     // Example: fetching website content, analyzing SEO aspects, etc.
 
+    const prompts = [
+      {
+        role: "system",
+        content:
+          "Act as a helpful expert in Search Engine Optimization (SEO), web data analysis, and marketing professional.",
+      },
+      {
+        role: "user",
+        content: `Analyze the curated website content ${websiteUrl} for SEO optimization and marketing insights, and provide recommendations to improve the website's search engine rankings and user experience.`,
+      },
+      // {
+      //   role: "assistant",
+      //   content:
+      //     "What are the current SEO practices implemented on the website?",
+      // },
+      // {
+      //   role: "assistant",
+      //   content: "How can the website improve its search engine rankings?",
+      // },
+      // {
+      //   role: "assistant",
+      //   content:
+      //     "What are the potential keywords that the website should target based on it's content?",
+      // },
+      // {
+      //   role: "assistant",
+      //   content: "What are the recommended on-page optimization techniques?",
+      // },
+      // {
+      //   role: "assistant",
+      //   content:
+      //     "How can the website improve its user experience and engagement?",
+      // },
+      // {
+      //   role: "assistant",
+      //   content:
+      //     "What are the effective digital marketing strategies for the website?",
+      // },
+    ];
     // Then, use OpenAI to generate insights
-    const response = await openai.createCompletion({
-      model: "gpt-4-vision-preview",
-      prompt: `Analyze the website at ${websiteUrl} for SEO optimization and marketing insights:`,
+    const response = await openai.chat.completions.create({
+      messages: prompts,
+      model:
+        //"gpt-3.5-turbo-1106",
+        // "gpt-4-32k",
+        // "gpt-4-1106-preview",
+        "gpt-4-vision-preview",
       max_tokens: 500,
+      // response_format: { type: "json_object" }, //this doesn't seem to work, even tho the docs say it's required..
     });
 
-    res.status(200).send({ insights: response.data.choices[0].text });
+    // const response = await openai.chat.completions.create.createCompletion({
+    //   model: "gpt-4-vision-preview",
+    //   prompt: `Analyze the website at ${websiteUrl} for SEO optimization and marketing insights:`,
+    //   max_tokens: 500,
+    // });
+
+    // console.log("response log", response.choices);
+
+    res.status(200).send(response.choices);
   } catch (error) {
     console.error("Error analyzing website:", error);
     res.status(500).send({ error: "Internal Server Error" });
@@ -46,14 +101,16 @@ async function fetchWebsiteContent(websiteUrl) {
 function analyzeSEO(content) {
   // Perform SEO analysis logic here
   // Example: analyze meta tags, headings, keywords, etc.
-  // Return SEO analysis results
+  // For now, let's just return a dummy SEO analysis result
+  return `SEO analysis for content: ${content.substring(0, 100)}...`;
 }
 
 // Function to analyze content and provide keyword recommendations
 function analyzeContent(content) {
   // Perform content analysis logic here
   // Example: analyze text for keyword density, readability, etc.
-  // Return keyword recommendations
+  // For now, let's just return a dummy keyword recommendation
+  return `Keyword recommendations for content: ${content.substring(0, 100)}...`;
 }
 
 // Function to perform website analysis
